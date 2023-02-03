59adaee73c80364ff26be2073c360e8cd7c63661
Ranjan02/ <ranjanmaji098@gmail.com>
2023-02-03 22:13:09 +0530

    DOM project

---
+++ DOM-Assisgnment-3/script.js
@@ -0,0 +1,19 @@
+let name = document.getElementsByClassName("userName");
+name[0].placeholder = 'FSJS 2.O'
+
+let mail = document.getElementsByClassName("userEmail");
+mail[0].placeholder = 'fsjs@ineuron.ai';
+
+let msg = document.getElementsByClassName("userMessage");
+msg[0].placeholder = 'Hello World';
+
+let name1 = document.getElementsByClassName("enterName");
+console.log(name1);
+name1[0].placeholder = 'FSJS 2.O'
+
+
+let mail2 = document.getElementsByClassName("enterMail");
+mail2[0].placeholder = 'fsjs@ineuron.ai';
+
+let msg2 = document.getElementsByClassName("enterMessage");
+msg2[0].placeholder = 'Hello World';
---
+++ DOM-Project-4/script.js
@@ -0,0 +1,5 @@
+let firstItam = document.getElementsByClassName('no-border');
+console.log(firstItam);
+// selectClashCard.style.color = 'red';
+firstItam.style.backgroundColor = 'red';
+// console.log(red);
