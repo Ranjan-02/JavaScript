680e676868ff3af06534cab51f3d1f6106ad7570
Ranjan02/ <ranjanmaji098@gmail.com>
2023-02-08 18:20:23 +0530

    practice add

---
+++ PRATICS/function.js
@@ -0,0 +1,124 @@
+// "use strict"
+// ====================================CALL BACK FUNCTION=============================
+// function myName (function2){
+//     console.log(function2);
+// }
+
+// function newFunction (){
+//     console.log("hii Ranjan how are you ?");
+// }
+
+// let function2 = newFunction ;
+
+
+
+
+// ====================================CALL BACK FUNCTION=============================
+
+// myName(function2);
+
+// function functionone(name){
+//     console.log(name);
+// }
+
+// function functionTwo(){
+//     alert("hellow world");
+// }
+// let functionStore = functionTwo;
+
+
+
+
+// functionone(functionStore);
+
+// ====================================CALL BACK FUNCTION=============================
+
+// let myFunction = function(paramiter){
+//     paramiter();
+// }
+// let functionNew = function(){
+//     console.log("hii i am Ranjan");
+// }
+// let functionStore = functionNew ;
+// console.log(functionNew);
+
+// =====================function dicration======================
+// console.log('---------------1-------------');
+// console.log('---------------2-------------');
+// ranjan();
+
+// console.log('---------------3-------------');
+// console.log('---------------5-------------');
+// console.log('---------------6-------------');
+// console.log('---------------7-------------');
+
+// ranjan();
+
+// console.log('---------------8-------------');
+// console.log('---------------9-------------');
+// console.log('---------------10-------------');
+
+
+
+
+// function ranjan(){
+//     console.log('hellow world');
+// }
+// ranjan();
+
+// =====================function Expresion======================
+// console.log('--------------------1--------------');
+// console.log('--------------------2--------------');
+// console.log('--------------------3--------------');
+// console.log('--------------------4--------------');
+// myFunction();
+
+// console.log('--------------------5-------------');
+// console.log('--------------------6--------------');
+// console.log('--------------------8--------------');
+// console.log('--------------------9--------------');
+// myFunction();
+
+// console.log('--------------------10--------------');
+// console.log('--------------------11-------------');
+
+// let myFunction = function(){
+//     console.log('My name Ranjan');
+// }
+// myFunction();
+
+
+// console.log('--------------------12-------------');
+// console.log('--------------------13-------------');
+// console.log('--------------------14-------------');
+
+// myFunction();
+// console.log('--------------------15-------------');
+
+
+// ===================normal functipon===================
+// function mynameFanction (){
+//   console.log('');"hellow world";
+// }
+// mynameFanction();
+// function myName(){
+//   console.log(myName);
+// }
+
+// let array = ["❤","😂","👍","🤦‍♀️","🤦‍♂️","🤷‍♀️"]
+// console.log(array.toString);
+//  let myFriendsName = ["rohim","anuska","dipanshu","kartik"];
+//  console.log(myFriendsName.toString);
+// console.log(myFriendsName.join(" + "));
+// console.log(myFriendsName.pop());
+// console.log(myFriendsName);
+// let newFriendsname = [];
+// console.log(myFriendsName.push("jamuna"));
+
+// console.log(myFriendsName);
+const myFriendsName = ["rohim","anuska","dipanshu","kartik"];
+// // console.log(myFriendsName.splice(1,0,"devjoti","amit"));
+// const a = myFriendsName.splice(2,0,"amit","rahul");
+// console.log(a);
+let newFunction = myFriendsName.splice(2,0,"Dipak","kailash");
+console.log(newFunction);
