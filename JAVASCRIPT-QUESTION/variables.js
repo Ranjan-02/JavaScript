cfb04d7b2aa0588faaebd5b9431447d2a412af4a
Ranjan02/ <ranjanmaji098@gmail.com>
2023-02-11 20:59:01 +0530

    add new

--- JAVASCRIPT-QUESTION/variables.js
+++ JAVASCRIPT-QUESTION/variables.js
@@ -3,20 +3,20 @@ console.log(myName);
 // 👇 output-----------
 // Ranjan
 
-//boolean
+//==========================boolean=======================
 let play_Cricket = true;
 console.log(play_Cricket);
 //   👇 output-----------
 // true
 
-//undefined
+//==================undefined=================
 let myFriendName;
 console.log(myFriendName);
 // 👇 output-----------
 // undefined
 
 
-//null
+//=====================null=======================
 let box = null;
 console.log(box);
 // 👇 output-----------
