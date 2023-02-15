be621fc717a6d072768ba39edd153124d3901c38
Ranjan02/ <ranjanmaji098@gmail.com>
2023-02-15 21:32:49 +0530

    add function core concept

---
+++ JAVASCRIPT-QUESTION/Question-13.js
@@ -0,0 +1,9 @@
+let age = window.prompt('enter your age');
+if (age > 18) {
+    console.log('you are old enough to drive');
+    
+}
+else{
+    console.log("you need to wait " + (18-age) +  " years to turn 18 to drive on road");
+    
+}
