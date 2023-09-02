17b4b82b660c1d07a436f307894b3ab89ac80b90
Ranjan02/ <ranjanmaji098@gmail.com>
2023-09-02 21:47:44 +0530

    new concept

--- javascript_concept/string.js
+++ javascript_concept/string.js
@@ -1,21 +1,17 @@
-// function rendomPassword() {
-//     let pass = ""
-//     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-//     for (let i = 1; i <= length; i++) {
-//         let char = Math.floor(Math.random() * str.length + 1)
-//         pass += str.charAt(char)
 
-//     }
-//     console.log(pass);
-//     console.log('ranjan');
-// }
-// rendomPassword();
+function passwordGender() {
 
-// for (let i = 1; i < 10; i++) {
-//     console.log(i);
-// }
-let letter = "abcdefghijklmn"
-console.log(letter.length);
-let rendomNumber = Math.floor(Math.random() * letter.length + 1);
-console.log(rendomNumber);
+    for (let i = 0; i < length; i++) {
+        let chreactar = "ABCDEFGHIJKLMNOPQRST";
+
+        console.log(i);
+    }
+
+
+
+
+
+}
+passwordGender();
+
