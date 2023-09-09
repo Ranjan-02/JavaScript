d38f9d1ab128a9e5a4ba5a750d0852f07b51eb76
Ranjan02/ <ranjanmaji098@gmail.com>
2023-09-09 21:56:37 +0530

    add new project

---
+++ javascript_concept/closer.js
@@ -0,0 +1,31 @@
+// function outer() {
+
+//     let myname = "Ranjan";
+//     function innerone() {
+//         console.log(myname);
+
+//     }
+//     return innerone;
+
+// }
+// let myfun = outer();//outer is refarance
+// myfun()//outer function excutuer
+
+// function lenguage() {
+
+
+//     let favleng = "javaScript"
+
+//     function mylenguage() {
+//         console.log(favleng);
+//     }
+//     mylenguage()
+
+
+
+
+// }
+
+// lenguage()
+
+
