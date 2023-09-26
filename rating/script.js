8e47416a3bd0ad6c8db7bc70cd3a734784d063f0
Ranjan02/ <ranjanmaji098@gmail.com>
2023-09-26 21:50:24 +0530

    add secript in ratting

---
+++ rating/script.js
@@ -0,0 +1,15 @@
+"use stric"
+
+document.querySelectorAll(".fa-star").forEach(function (element) {
+    element.addEventListener("click", function () {
+        displayYourRatting()
+    })
+})
+
+function displayYourRatting() {
+
+    document.querySelectorAll(".fa-star").forEach(function (iteam) {
+
+    })
+}
+
