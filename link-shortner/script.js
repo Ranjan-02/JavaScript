f9e049c0db0c230a2fd2d94ba2c052a21b62750c
Ranjan02/ <ranjanmaji098@gmail.com>
2023-09-28 19:05:41 +0530

    modify js file

--- link-shortner/script.js
+++ link-shortner/script.js
@@ -3,28 +3,22 @@ const btnEnter = document.querySelector(".linkdiv button");
 const copyInput = document.querySelector(".copy input");
 const copyBtn = document.querySelector(".copy button");
 
+btnEnter.addEventListener("click", function () {
+    if (!linkInput.value) {
 
+        return
+    }
+    fetchLinkShortner(linkInput.value)
+})
 
-btnEnter.addEventListener("click", fetchLinkShortner)
-
-
-async function fetchLinkShortner() {
-
-    let value = linkInput.value;
-    console.log(value);
-
+async function fetchLinkShortner(value) {
     let data = await fetch(`https://api.shrtco.de/v2/shorten?url=${value}`)
     data = await data.json()
-    console.log(data.result);
     copyInput.value = data.result.short_link;
-
-
-    copyBtn.addEventListener("click", function () {
-        copyInput.select()
-        navigator.clipboard.writeText(copyInput.value)
-
-        console.log('select btn');
-    })
-
-
 }
+
+copyBtn.addEventListener("click", function () {
+    copyInput.select()
+    navigator.clipboard.writeText(copyInput.value)
+
+})
