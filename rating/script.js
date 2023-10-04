f7ebae6e2fb33a94b444318c2991c07d15fe8649
Ranjan02/ <ranjanmaji098@gmail.com>
2023-10-04 18:21:59 +0530

    complite ratting

--- rating/script.js
+++ rating/script.js
@@ -1,15 +1,33 @@
 "use stric"
+const stars = document.querySelectorAll(".fa-star")
 
-document.querySelectorAll(".fa-star").forEach(function (element) {
-    element.addEventListener("click", function () {
-        displayYourRatting()
+const ratting = [
+    { emoji: "😍", text: "I love It" },
+    { emoji: "😒", text: "bad" },
+    { emoji: "😃", text: "not Bad" },
+    { emoji: "😢", text: "very bad" },
+    { emoji: "🤦‍♂️", text: "Excilent" },
+]
+stars.forEach(function (star, index1) {
+    star.addEventListener("click", function () {
+
+        document.querySelector(".emoji").textContent = ratting[index1].emoji
+        document.querySelector(".status").textContent = ratting[index1].text
+        stars.forEach(function (star, index2) {
+
+            if (index1 >= index2) {
+                star.classList.add("active")
+                console.log('clicked');
+            } else if (star.classList.contains("active")) {
+                star.classList.remove("active")
+            }
+        })
     })
+
 })
 
-function displayYourRatting() {
 
-    document.querySelectorAll(".fa-star").forEach(function (iteam) {
 
-    })
-}
+
+
 
