9195aaff809a57ef69ae40b0db46be4f3899f6cc
Ranjan02/ <ranjanmaji098@gmail.com>
2023-10-07 18:32:41 +0530

    modify news app

--- news-app/script.js
+++ news-app/script.js
@@ -1,10 +1,6 @@
 const APIKEY = "2807ee8616c04f17bed6686b59faf4e5"
 const URL = "https://newsapi.org/v2/everything?q="
 
-window.addEventListener("load", function () {
-
-    fetchNews("today");
-});
 
 async function fetchNews(qurry) {
     const resp = await fetch(`${URL}${qurry}&apiKey=${APIKEY}`)
@@ -64,3 +60,4 @@ document.querySelector(".btn-enter").addEventListener("click", function () {
 
 
 })
+fetchNews("today");
