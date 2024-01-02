576443fe743be8f9ed03616be792a0fdb32d88ee
Ranjan02/ <ranjanmaji098@gmail.com>
2024-01-02 23:27:49 +0530

    add new project

--- dictioanary/script.js
+++ dictioanary/script.js
@@ -4,7 +4,7 @@ document.querySelector("button").addEventListener("click", function (e) {
 
     const word = input.value
     if (word === "") {
-        return;
+        alert("please Enter Value")
     } else {
         getData(word)
     }
@@ -15,23 +15,26 @@ async function getData(word) {
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+
+    if (data.title) {
+        document.querySelector(".text").innerHTML = "<h1>Data not found</h1>";
+    }
 
 
+
+    document.querySelector(".text").innerHTML = "";
     const createDiv = document.createElement("div")
-
     createDiv.innerHTML =
         `
-   <section class="data-contenar">
+   
         <h3> ${data[0].word}</h3>
         <p class="discription"> Discription : <span> ${data[0].meanings[0].definitions[0].definition}</span></p>
         <p>Parts of speech : <span>${data[0].meanings[0].partOfSpeech == undefined ? "Data not found" : data[0].meanings[0].partOfSpeech}</span> </p>
         <button ><a href="${data[0].sourceUrls}"  target="_blank"> Read more</a> </button>
-    </section>
+
     `
-    document.querySelector(".inputBox").append(createDiv)
+    document.querySelector(".text").appendChild(createDiv)
 
 }
-
 
 
